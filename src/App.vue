d4785cd6431377fc9894b80de85d5ea71060927c
wkra <wkrawiec@gmail.com>
2023-03-27 16:19:26 +0200

    event added

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 <script setup>
 import { useCookies } from "vue3-cookies";
 import TheList from "./components/TheList.vue";
+import { onMounted } from "vue";
 
 const itemKey = "item";
 const { cookies } = useCookies();
@@ -19,6 +20,12 @@ const { cookies } = useCookies();
 const cookieItem = cookies.get(itemKey);
 const localStorageItem = localStorage.getItem(itemKey) || "none";
 const sessionStorageItem = sessionStorage.getItem(itemKey) || "none";
+
+onMounted(() => {
+  document.addEventListener("myevent", function () {
+    console.log("myevent");
+  });
+});
 </script>
 
 <style></style>
