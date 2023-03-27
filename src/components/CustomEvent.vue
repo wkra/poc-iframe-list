2f6779d0e537d3de13d7a2ac04e4128525c8ffb9
wkra <wkrawiec@gmail.com>
2023-03-27 23:06:34 +0200

    popmessage added

--- src/components/CustomEvent.vue
+++ src/components/CustomEvent.vue
@@ -2,6 +2,7 @@
   <div>
     <div>TEST child / parent communication</div>
     <button type="button" @click="emitHandler">Emit event</button>
+    <button type="button" @click="emitPostMessage">Emit postMessage</button>
   </div>
 </template>
 <script setup>
@@ -22,11 +23,29 @@ const eventHandler = () => {
   alert("I am script / iframe. Parent event received");
 };
 
+const popMessageHandler = (event) => {
+  const urls = [
+    "http://localhost:8081",
+    "http://localhost:8080",
+    "https://wkra.github.io/poc-iframe-list",
+    "https://wkra.github.io/poc-iframe-container",
+  ];
+
+  if (!urls.includes(event.origin)) return;
+  if (event.data.myCustomParent) alert(event.data.message)
+};
+
+const emitPostMessage = () => {
+  window.parent.postMessage({myCustomChild: true, message: 'message from child'}, "*");
+};
+
 onMounted(() => {
   document.addEventListener("parentevent", eventHandler);
+  window.addEventListener("message", popMessageHandler);
 });
 
 onUnmounted(() => {
   document.removeEventListener("parentevent", eventHandler);
+  window.removeEventListener("message", popMessageHandler);
 });
 </script>
