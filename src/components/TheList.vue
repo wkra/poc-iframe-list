5bef5ad100fe93352e1914c8842e7d8de4e958f5
wkra <wkrawiec@gmail.com>
2023-03-27 16:10:21 +0200

    load more items

--- src/components/TheList.vue
+++ src/components/TheList.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    THE LIST
+    <hr />
+    <div>THE LIST</div>
+    <div>
+      <button type="button" v-if="nextUrl" @click="loadMoreHandler">
+        load more
+      </button>
+    </div>
     <ul>
       <li v-for="item in list" :key="item.name">
         {{ item.name }}
@@ -12,16 +18,30 @@
 <script setup>
 import { ref, onMounted } from "vue";
 const list = ref([]);
+const apiUrl = 'https://swapi.dev/api/people';
+const nextUrl = ref('');
 
-const getPeople = async (url = "https://swapi.dev/api/people") => {
+const loadMoreHandler = () => {
+  addPeople(apiUrl.value);
+}
+
+const getPeople = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.results)
+  nextUrl.value = data.next
 
   return data.results;
 };
 
-onMounted(async () => {
-  list.value = await getPeople();
+const addPeople = async (url = apiUrl) => {
+  const result = await getPeople(url);
+
+  result.forEach(element => {
+    list.value.push(element)
+  });
+}
+
+onMounted(() => {
+  addPeople(apiUrl)
 });
 </script>
