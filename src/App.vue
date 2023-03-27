56a483944290cf6185a34031d811d463d738f249
wkra <wkrawiec@gmail.com>
2023-03-27 13:49:14 +0200

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <div>Cookie item: {{ cookieItem }}</div>
+      <div>localStore item: {{ localStorageItem }}</div>
+      <div>sessionStore item: {{ sessionStorageItem }}</div>
+    </div>
+    <TheList />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { useCookies } from "vue3-cookies";
+import TheList from "./components/TheList.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const itemKey = "item";
+const { cookies } = useCookies();
+
+const cookieItem = cookies.get(itemKey);
+const localStorageItem = localStorage.getItem(itemKey) || "none";
+const sessionStorageItem = sessionStorage.getItem(itemKey) || "none";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/TheList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    THE LIST
+    <ul>
+      <li v-for="item in list" :key="item.name">
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+const list = ref([]);
+
+const getPeople = async (url = "https://swapi.dev/api/people") => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.results)
+
+  return data.results;
+};
+
+onMounted(async () => {
+  list.value = await getPeople();
+});
+</script>
